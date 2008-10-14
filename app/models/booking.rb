142dcc914566caae3966e113e7f60aa1cc4f3e21
Peter Kordel <pkordel@gmail.com>
2008-10-14 23:43:23 +0200

    Minor changes

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,8 @@ class Booking < ActiveRecord::Base
   belongs_to :person
   
   # This allows us to say Booking.active and @booking.active and 
-  # receive an array of all bookings that end on a given date.
-  named_scope :active, lambda { |*args| {:conditions => ["start_date < ? AND end_date > ?", (args.first || Time.now), (args.last || Time.now)]} }
+  # receive an array of all bookings that start and end on a given date.
+  named_scope :active, lambda { |*args| {:conditions => ["start_date < ? AND end_date > ?", 
+                                                          (args.first || Time.now), 
+                                                          (args.last || Time.now)]} }
 end
