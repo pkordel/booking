ae128b774a028b2f65f28178619167e94fff0f4f
Peter Kordel <pkordel@gmail.com>
2008-10-14 19:45:27 +0200

    Support for date ranges in active booking finder

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,5 @@ class Booking < ActiveRecord::Base
   
   # This allows us to say Booking.active and @booking.active and 
   # receive an array of all bookings that end on a given date.
-  named_scope :active, lambda { |*args| {:conditions => ["end_date > ?", (args.first || Time.now)]} }
+  named_scope :active, lambda { |*args| {:conditions => ["start_date < ? AND end_date > ?", (args.first || Time.now), (args.last || Time.now)]} }
 end
